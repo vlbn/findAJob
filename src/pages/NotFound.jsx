a8a10b8ebc48e1637f641b0fb553580302b4619a
valbuena <valbuena@duck.com>
2024-07-04 20:33:58 +0200

    fragment OK

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
   const recortado = location.pathname.substring(0, 10) + "..";
   return (
-    <div>
+    <>
       <Link to="/" className="text-4xl grid h-screen place-items-center">
         <div className="inline-flex">
           😒 <div className="font-bold mx-2"> {recortado} </div>?
         </div>
       </Link>
-    </div>
+    </>
   );
 };
 
